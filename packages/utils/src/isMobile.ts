73f677fdbc66d70ff66f3ddfa41ac8c29d671262
陶金超 <1296804291@qq.com>
2022-08-22 20:44:05 +0800

    初始化函数库目录结构

---
+++ packages/utils/src/isMobile.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: taojinchao
+ * @Date: 2022-08-22 20:40:19
+ * @LastEditors: taojinchao
+ * @LastEditTime: 2022-08-22 20:41:50
+ * @Description: 入口文件
+ */
+export * from "./isMobile";
